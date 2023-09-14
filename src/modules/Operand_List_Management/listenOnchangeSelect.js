42a5e9226e65b9703f9f5a4e0560b87c95bba91c
Saqib Bhatti <66674431+SQZ0111@users.noreply.github.com>
2023-09-14 18:28:46 +0200

    Delete src/modules/Operand_List_Management/listenOnchangeSelect.js

--- src/modules/Operand_List_Management/listenOnchangeSelect.js
+++
@@ -1,9 +0,0 @@
-import { storeSelectValue } from "../Storage_and_Retrieval/storeSelectedValueInLocalStorage.js";
-
-function listenChangeStoreValue () {
-    const operatorList = document.querySelector('.operatorList');
-
-    operatorList.addEventListener("change",storeSelectValue);
-}
-
-export {listenChangeStoreValue};
